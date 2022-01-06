cc8dd79dca09deb30df2538f6b65dbaa17bc6ae1
ericlin7 <80200462+ericlin7@users.noreply.github.com>
2022-01-05 23:00:47 -0800

    add

--- app.py
+++ app.py
@@ -24,9 +24,21 @@ driver.find_element(By.ID,'password').send_keys(Keys.RETURN)
 
 driver.get("https://www.linkedin.com/jobs/")
 time.sleep(3)
+
 # find the keywords/location search bars:
 search_keywords = driver.find_element(By.ID,'jobs-search-box-keyword-id-ember30')
 search_keywords.send_keys(keywords)
     
 search_location = driver.find_element(By.ID,'jobs-search-box-location-id-ember30')
 search_location.send_keys(location + Keys.ENTER)
+
+list_items = driver.find_elements_by_class_name("occludable-update")
+for job in list_items:
+    # executes JavaScript to scroll the div into view
+    driver.execute_script("arguments[0].scrollIntoView();", job)
+    job.click()
+    time.sleep(3)
+    # get info:
+    [position, company, location] = job.text.split('\n')[:3]
+    details = driver.find_element_by_id("job-details").text
+    # do what you want with that info...
