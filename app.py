af1eca387b36f45cebe95893e027f9ced24c8bce
stian34556 <jytian34556@gmail.com>
2022-01-05 22:51:46 -0800

    obtain results from search bar

--- app.py
+++ app.py
@@ -1,31 +1,32 @@
 from selenium import webdriver
 import time
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
 
 # direct webdriver where the browser file is:
-driver_path = "geckodriver.exe"
-driver = webdriver.Firefox(executable_path=driver_path)
+s = Service('/Users/jiayuetian/Downloads/chromedriver')
+driver = webdriver.Chrome(service=s)
 
 # your secret credentials:
 email = "wangmaria923@gmail.com"
-password = "74$ppkt!"
+password = "74$ppkt123"
 
-# login linkedin
+keywords = "Software"
+location = "Toronto"
+
+# Go to linkedin and login
 driver.get('https://www.linkedin.com/login')
 time.sleep(3)
-driver.find_element_by_id('username').send_keys(email)
-driver.find_element_by_id('password').send_keys(password)
-driver.find_element_by_id('password').send_keys(Keys.RETURN)
-
+driver.find_element(By.ID, 'username').send_keys(email)
+driver.find_element(By.ID,'password').send_keys(password)
+driver.find_element(By.ID,'password').send_keys(Keys.RETURN)
 
 driver.get("https://www.linkedin.com/jobs/")
 time.sleep(3)
 # find the keywords/location search bars:
-search_bars = driver.find_elements_by_class_name('jobs-search-box__text-input')
-search_keywords = search_bars[0]
+search_keywords = driver.find_element(By.ID,'jobs-search-box-keyword-id-ember30')
 search_keywords.send_keys(keywords)
     
-search_location = search_bars[2]
-search_location.send_keys(location)
-search_location.send_keys(Keys.RETURN)
-
+search_location = driver.find_element(By.ID,'jobs-search-box-location-id-ember30')
+search_location.send_keys(location + Keys.ENTER)
