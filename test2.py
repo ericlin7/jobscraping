809922ed7cd0e83ad09b3594deeb54a66cb9be2e
ericlin7 <80200462+ericlin7@users.noreply.github.com>
2022-01-07 23:14:40 -0800

    python sstring thinngy

---
+++ test2.py
@@ -0,0 +1,23 @@
+skills = ["Scala, Java, React/Redux, HTML, CSS, JavaScript"]
+
+des = """Responsibilities:
+Support the engineering team with software development; design and implement scalable systems and infrastructures
+Measure our work thoroughly, analyze results, and generate hypotheses to drive new product changes through A/B testing
+Setup monitoring and testing of our APIs and services to better understand real time health of our products
+Code software using Scala and Java
+Implement web interfaces using React/Redux, HTML, CSS, and JavaScript
+Work in a cross functional team of engineers, PMs, designers, and UX researchers
+
+Basic Qualifications:
+Currently pursuing a B.A./B.S. Degree or higher in Computer Science, or related technical field
+Must be currently enrolled in a full time degree program and returning to the program after the completion of the internship
+
+Preferred Qualifications:
+Strong competencies in data structures, algorithms and software design
+Demonstrated software engineer experience via an internship, work experience, school projects or coding competitions.
+Self-motivated, smart, hands-on, relentlessly focused on impact, and able to operate under a great deal of independence
+Expected to complete degree by December 2023 or earlier"""
+
+for a in skills:
+    if des.find(a):
+        print(a)
